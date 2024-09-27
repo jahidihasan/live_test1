fbe1540578f8f80f7146da1d55b6221261336201
Jahidi Hasan <jahidihasanswe3767@gmail.com>
2024-09-27 23:27:02 +0600

    Initial commit

---
+++ bin/live_test1.dart
@@ -0,0 +1,5 @@
+import 'package:live_test1/live_test1.dart' as live_test1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${live_test1.calculate()}!');
+}
---
+++ test/live_test1_test.dart
@@ -0,0 +1,8 @@
+import 'package:live_test1/live_test1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
